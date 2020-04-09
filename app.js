c7bce52944d6636e2a9b0b4ecb715916b4933f4a
keroro-o <obata19760205akinobu@outlook.jp>
2020-04-09 13:23:26 +0900

    helmet を利用して、セキュリティに関する対策を行う

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(helmet());    // appというオブジェクトのuse関数を使って、helmetを使うように登録する
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
